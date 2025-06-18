7a931e271d1fe06893211b0c98a34293c0bd6f73
devankit1994 <devankit1994@gmail.com>
2025-06-18 17:55:54 +0530

    Added Header Component

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import logo from "../assets/logo.png";
+
+export default function Header() {
+  return (
+    <header>
+      <img src={logo} alt="Yumly Logo" />
+      <h1>Yumly</h1>
+    </header>
+  );
+}
